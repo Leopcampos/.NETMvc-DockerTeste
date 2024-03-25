2f2df905beace0dfee23729d328a9f0715854082
Leo Campos <leopcampos85@gmail.com>
2024-03-25 19:11:25 -0300

    Criando o controller, as Views de Index e Create e aplicando a Migration no Docker.

--- src/Livraria/Program.cs
+++ src/Livraria/Program.cs
@@ -1,7 +1,23 @@
+using Livraria.Models;
+using Livraria.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+var server = builder.Configuration["DbServer"] ?? "localhost";
+var port = builder.Configuration["DbPort"] ?? "1450";
+var user = builder.Configuration["DbUser"] ?? "SA";
+var password = builder.Configuration["Password"] ?? "123@Mudar";
+var database = builder.Configuration["Database"] ?? "LivrosDb";
+
+var connectionString = $"Server={server}, {port}; Initial Catalog={database}; User ID={user}; Password={password};";
+
+var conexao = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
@@ -12,6 +28,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+DatabaseManagementService.MigrationInitialisation(app);
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
